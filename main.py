6177bd022fc77c969568ceb7412ced6f028abeca
KUSUMA GOTTIPATI <kusumagottipati6503@gmail.com>
2025-10-31 18:19:57 +0530

    commit

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://choose-your-own-adventure-frontend.vercel.app/"],
+    allow_origins=["https://choose-your-own-adventure-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
